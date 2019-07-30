00e3658d64a277bbbfa60114df64cafff095fa28
muhammad rahman <rahman9600@gmail.com>
2019-07-30 10:24:08 +0700

    Update AppDelegate.m
    adding GeoFencing as callbacks function

--- ios/Runner/AppDelegate.m
+++ ios/Runner/AppDelegate.m
@@ -3,8 +3,18 @@
 
 @implementation AppDelegate
 
+void registerPlugins(NSObject<FlutterPluginsRegistery> registery) {
+  [GeneratedPluginRegistrant registerWithRegistry:registery]
+}
+
+@implementation AppDelegate
+
 - (BOOL)application:(UIApplication *)application
     didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+
+  registerPlugins(self);
+
+  [GeofencingPlugin setPluginRegistrantCallback:registerPlugins];
   [GeneratedPluginRegistrant registerWithRegistry:self];
   // Override point for customization after application launch.
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
